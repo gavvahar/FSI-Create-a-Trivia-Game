782330a5773943dc47a2a6bfe89803a29311f0d5
Nihar Gavva <nihargavva@gmail.com>
2022-04-25 18:12:49 +0000

    finished with the bonus

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 let userName = window.prompt('Please enter your name: ')
 let userScore = 0
-for(let i = 0; i < questions.length; i++)
+let playAgain = true
+function playGame()
 {
+  for(let i = 0; i < questions.length; i++)
+  {
     let question = questions[i]
     let userAnswer = window.prompt(question.text)
     if(userAnswer === question.correctAnswer)
     {
         userScore = userScore + 10
     }
+  }
+  window.alert('Your score is: ' + userScore)
 }
-window.alert('Your score is: ' + userScore)
+while (playAgain === true)
+{
+  playGame()
+  let userChoice = window.prompt('Would you like to play the game again? Answer yes or no.')
+  if(userChoice === 'yes')
+  {
+    playAgain = true
+  } else
+  {
+    playAgain = false
+    window.alert('Thanks for playing the game!')
+  }
+}
